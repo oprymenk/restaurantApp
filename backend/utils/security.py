7b4ce1b52dedf165968486f37ccb85142f67f5a9
oprymenk <161228024+oprymenk@users.noreply.github.com>
2026-04-27 20:45:58 +0300

    Update security.py

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -4,10 +4,13 @@ from jose import JWTError, jwt
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
-# JWT
 SECRET_KEY = "super_secret_key_change_this"
+REFRESH_SECRET_KEY = "super_refresh_secret_key_change_this"
+
 ALGORITHM = "HS256"
+
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -15,16 +18,26 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: dict, expires_delta: int = ACCESS_TOKEN_EXPIRE_MINUTES):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, REFRESH_SECRET_KEY, algorithm=ALGORITHM)
 
 def decode_access_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        return None
+
+def decode_refresh_token(token: str):
+    try:
+        return jwt.decode(token, REFRESH_SECRET_KEY, algorithms=[ALGORITHM])
     except JWTError:
         return None
